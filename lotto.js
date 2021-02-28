20edfa3ca53cbe595fc59c1668e277fb83b7d81e
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-02-28 21:41:01 +0900

    Update lotto.js

--- lotto.js
+++ lotto.js
@@ -1,3 +1,14 @@
 function drawing() {
-
+    var numbers = [];
+    for (var i = 0; i < 6; i++) {
+        var ran = Math.floor(Math.random() * 45) + 1;
+        if (numbers.indexOf(ran) === -1) {
+            numbers.push(ran);
+        } else {
+            i -= 1;
+        }
+    }
+    for (var i = 0; i < 6; i++) {
+        document.write(numbers[i] + " ");
+    }
 }
