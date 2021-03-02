26cacaf1f1cc26c050a76642395ccdb0a92c5eaf
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-03-02 22:35:39 +0900

    Update lotto.js

--- lotto.js
+++ lotto.js
@@ -8,7 +8,13 @@ function drawing() {
             i -= 1;
         }
     }
-    for (var i = 0; i < 6; i++) {
-        document.write(numbers[i] + " ");
-    }
+    document.getElementById("ball1").innerHTML = numbers[0];
+    document.getElementById("ball2").innerHTML = numbers[1];
+    document.getElementById("ball3").innerHTML = numbers[2];
+    document.getElementById("ball4").innerHTML = numbers[3];
+    document.getElementById("ball5").innerHTML = numbers[4];
+    document.getElementById("ball6").innerHTML = numbers[5];
+
+
+
 }
